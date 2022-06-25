c3d7ce39352b739b8e2fcb706fcede0cb2f523ff
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2022-06-25 13:26:25 +0700

    refactor: better `DataFaucet` signature

--- src/data-faucet.ts
+++ src/data-faucet.ts
@@ -1,3 +1,4 @@
 import { DataSink } from './data-sink.js';
 
-export type DataFaucet<out TOut, in TIn> = (this: void, arg: TIn, sink: DataSink<TOut>) => Promise<void>;
+export type DataFaucet<out TOut, in TIn extends unknown[]> =
+    (this: void, ...args: [...TIn, DataSink<TOut>]) => Promise<void>;

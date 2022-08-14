add3985034971df3f721e95db3eb76059a0e56c1
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2022-08-14 10:49:54 +0700

    refactor: rename to data mix

--- src/mix/data-seep.ts
+++ src/mix/data-seep.ts
@@ -2,5 +2,5 @@ import { IntakeFaucet } from '../data-faucet.js';
 import { DataInfusion } from '../data-infusion.js';
 import { DataMix } from './data-mix.js';
 
-export type DataSeep<in out T, out TOptions extends unknown[] = [], TMix extends DataMix = DataMix> =
+export type DataSeep<in out T, out TOptions extends unknown[] = [], in TMix extends DataMix = DataMix> =
     (infusion: DataInfusion<T, TOptions>, mix: TMix) => IntakeFaucet<T>;

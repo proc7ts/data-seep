e012b8b359325f99152cc38197f598637557747f
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2022-08-14 10:48:50 +0700

    refactor: rename to data mix

--- src/mix/mod.ts
+++ src/mix/mod.ts
@@ -1,6 +1,6 @@
-export * from './data-mixer.js';
 export * from './data-compound.js';
 export * from './data-mix.js';
+export * from './data-mixer.js';
 export * from './data-seep.js';
 export * from './data-sink.error.js';
 export * from './default-data-mix.js';

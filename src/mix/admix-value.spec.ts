26f0a4a486203c43b2db700f6a13e5da0290c33d
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2022-09-24 14:44:27 +0700

    Infuse mixed data with `DataAdmix` instances

--- src/mix/admix-value.spec.ts
+++ src/mix/admix-value.spec.ts
@@ -1,14 +1,14 @@
 import { describe, expect, it } from '@jest/globals';
 import { DataFaucet } from '../data-faucet.js';
 import { withValue } from '../with-value.js';
+import { admixValue } from './admix-value.js';
 import { DataMixer } from './data-mixer.js';
-import { seepValue } from './seep-value.js';
 
-describe('seepValue', () => {
-  it('seeps the given value', async () => {
+describe('admixValue', () => {
+  it('pours the given value', async () => {
     const mixer = new DataMixer();
 
-    mixer.infuse(withTestData, seepValue(123));
+    mixer.add(admixValue(withTestData, 123));
 
     let sank: number | undefined;
 

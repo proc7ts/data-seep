39d1f8009a52289c4a41d4a9ba2d1f9691440b3e
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2022-10-17 17:07:23 +0700

    Optimize `shareSeep()` implementation

--- src/seeps/share.seep.ts
+++ src/seeps/share.seep.ts
@@ -1,4 +1,5 @@
 import { neverSupply, Supply, SupplyOut } from '@proc7ts/supply';
+import { DataFaucet, IntakeFaucet } from '../data-faucet.js';
 import { DataSeep } from '../data-seep.js';
 import { DataSink } from '../data-sink.js';
 
@@ -13,46 +14,48 @@ import { DataSink } from '../data-sink.js';
  * @returns New data seep.
  */
 export function shareSeep<T>(): DataSeep<T> {
-  return input => {
-    const sinks = new Map<Supply, DataSink<T>>();
-    let outputSupply = neverSupply();
-    const sinkInput = async (value: T): Promise<void> => {
-      await Promise.all([...sinks.values()].map(async sink => await sink(value)));
-    };
-    const removeSink = (supply: Supply): void => {
-      sinks.delete(supply);
-      if (!sinks.size) {
-        outputSupply.done();
+  return sharingSeep;
+}
+
+function sharingSeep<T>(input: IntakeFaucet<T>): DataFaucet<T> {
+  const sinks = new Map<Supply, DataSink<T>>();
+  let outputSupply = neverSupply();
+  const sinkInput = async (value: T): Promise<void> => {
+    await Promise.all([...sinks.values()].map(async sink => await sink(value)));
+  };
+  const removeSink = (supply: Supply): void => {
+    sinks.delete(supply);
+    if (!sinks.size) {
+      outputSupply.done();
+    }
+  };
+
+  return async (sink: DataSink<T>, sinkSupply: SupplyOut = new Supply()): Promise<void> => {
+    const supply = new Supply();
+
+    supply.whenOff(() => removeSink(supply));
+    sinkSupply.alsoOff(supply);
+
+    if (!supply.isOff) {
+      const isFirstSink = !sinks.size;
+
+      sinks.set(supply, sink);
+
+      if (isFirstSink) {
+        outputSupply = new Supply(() => {
+          outputSupply = neverSupply();
+        });
+
+        input(sinkInput, outputSupply)
+          .then(() => {
+            outputSupply.done();
+          })
+          .catch(error => outputSupply.fail(error));
       }
-    };
 
-    return async (sink: DataSink<T>, sinkSupply: SupplyOut = new Supply()): Promise<void> => {
-      const supply = new Supply();
+      outputSupply.alsoOff(supply);
+    }
 
-      supply.whenOff(() => removeSink(supply));
-      sinkSupply.alsoOff(supply);
-
-      if (!supply.isOff) {
-        const isFirstSink = !sinks.size;
-
-        sinks.set(supply, sink);
-
-        if (isFirstSink) {
-          outputSupply = new Supply(() => {
-            outputSupply = neverSupply();
-          });
-
-          input(sinkInput, outputSupply)
-            .then(() => {
-              outputSupply.done();
-            })
-            .catch(error => outputSupply.fail(error));
-        }
-
-        outputSupply.alsoOff(supply);
-      }
-
-      await supply.whenDone();
-    };
+    await supply.whenDone();
   };
 }

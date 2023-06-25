a9a9f36ebe69aac52ebccd0ae13429ec485fd5ea
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2023-06-25 14:18:53 +0700

    Get rid of data mix

--- src/mix/mod.ts
+++
@@ -1,7 +0,0 @@
-export * from './admixes/mod.js';
-export * from './blended.admix.js';
-export * from './data-admix.js';
-export * from './data-mix.js';
-export * from './data-mixer.js';
-export * from './default-data-mix.js';
-export * from './single.admix.js';
--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,4 @@
 export * from './data-faucet.js';
-export * from './data-infusion.js';
 export * from './data-seep.js';
 export * from './data-sink.js';
 export * from './drains/mod.js';
@@ -7,7 +6,6 @@ export * from './faucet.js';
 export * from './faucets/mod.js';
 export * from './infusions/mod.js';
 export * from './joints/mod.js';
-export * from './mix/mod.js';
 export * from './seeps/mod.js';
 export * from './sink-all.js';
 export * from './sink.js';
